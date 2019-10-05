68e18ee2c5d58c1bafd4ecc6bb79a0c605ee0659
jay <jaywaynejuly@gmail.com>
2019-10-05 12:48:32 -0400

    third commit add one more hello

--- src/utilities/AlertHandler.java
+++ src/utilities/AlertHandler.java
@@ -5,5 +5,6 @@ public class AlertHandler {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Teklab Students!\n You rock");
+		System.out.println("hello again");
 	}
 }
